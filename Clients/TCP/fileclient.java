f10cd769045fd5145da1521944b70f509271eefb
Gerhard van Wyk <21091137@student.g.nwu.ac.za>
2018-10-19 00:34:25 +0200

    Updates

--- Clients/TCP/fileclient.java
+++ Clients/TCP/fileclient.java
@@ -1,23 +1,29 @@
-/* File Client *********************************************************/
-
+//Imports needed for the TCP client side for a text file transfer
 import java.io.*;
 import java.net.*;
 import java.util.Timer;
 
+//Class created for file client 
 public class fileclient{
+	//Main method created for TCP client
 	public static void main(String[] argv) throws Exception {
+		//Creating and starting timer
 		long startTime = System.currentTimeMillis();
 		long elapsedTime;
 		byte []b=new byte[2002];
+		//Creating the socket with server andress and port number
 		Socket sr=new Socket("192.168.8.102",5643);
+		//Creating the input stream to write the incoming text file.
 		InputStream is=sr.getInputStream();
+		//Setting the patch for the file to be written to
 		FileOutputStream fr=new FileOutputStream("C:/Users/gvanw/Desktop/Jaar 3/Semester 2/ITRW 322 - Networks/Reports/Examples JAVA/itrw322-javaTCP/client.txt");
 		is.read(b,0,b.length);
 		fr.write(b, 0, b.length);
-		
+		//Print ou the length
 		System.out.println("Bytes: " + b.length);
 		
-		elapsedTime = System.currentTimeMillis() - startTime ;
+		elapsedTime = System.currentTimeMillis() - startTime;
+		//Print ou the time that has elapsed
 		System.out.println("Time: " + elapsedTime + "ms");
 	}
 }
--- Servers/TCP/fileserver.java
+++ Servers/TCP/fileserver.java
@@ -1,21 +1,19 @@
-/*TCP SERVER*
-JACQUES VOSLOO
-27236285
-ITRW322
-2018/10/18
-*/
-
+//Import data 
 import java.io.*;
 import java.net.*;
 
+//Creating the class for the TCP server
 public class fileserver {
-public static void main(String[] args) throws Exception {
-ServerSocket s=new ServerSocket(5643);
-Socket sr=s.accept();
-FileInputStream fr=new FileInputStream("C:/Users/Jacques/Desktop/itrw322-javaTCP/server.txt");
-byte b[]=new byte[2002];
-fr.read(b, 0, b.length);
-OutputStream os=sr.getOutputStream();
-os.write(b, 0, b.length);
-}
+	//Creating the main method for the TCP server
+	public static void main(String[] args) throws Exception {
+		//Creating the socket to send the text file on the port number
+		ServerSocket s=new ServerSocket(5643);
+		Socket sr=s.accept();
+		//Path to the file you want to send
+		FileInputStream fr=new FileInputStream("C:/Users/Jacques/Desktop/itrw322-javaTCP/server.txt");
+		byte b[]=new byte[2002];
+		fr.read(b, 0, b.length);
+		OutputStream os=sr.getOutputStream();
+		os.write(b, 0, b.length);
+	}
 }
